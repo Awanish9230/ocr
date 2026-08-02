34ee14d8f04a71dc0be4b4600c4e78ca3b712d1a
Awanish Verma <a2023cse9230@imsec.ac.in>
2026-08-02 12:16:21 +0530

    Fix CORS for Vercel deployment

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,12 @@ app = FastAPI(
 # Set all CORS enabled origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://192.168.1.35:3000"],
+    allow_origins=[
+        "http://localhost:3000", 
+        "http://127.0.0.1:3000", 
+        "http://192.168.1.35:3000",
+        "https://ocr-nu-bice.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
